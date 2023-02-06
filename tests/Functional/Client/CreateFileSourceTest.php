6d728d40f87753fa64ffba32475bff4f53b774e5
Jon Cram <webignition@gmail.com>
2023-02-06 16:55:07 +0000

    Add functional tests for file source creation

---
+++ tests/Functional/Client/CreateFileSourceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\SourcesClient\Tests\Functional\Client;
+
+use GuzzleHttp\Psr7\Response;
+use SmartAssert\SourcesClient\Model\FileSource;
+use SmartAssert\SourcesClient\Model\InvalidRequestError;
+
+class CreateFileSourceTest extends AbstractClientModelCreationTestCase
+{
+    public function testCreateFileSourceRequestProperties(): void
+    {
+        $label = 'job label';
+
+        $this->mockHandler->append(new Response(
+            200,
+            ['content-type' => 'application/json'],
+            (string) json_encode([
+                'id' => md5((string) rand()),
+                'user_id' => md5((string) rand()),
+                'type' => 'file',
+                'label' => $label,
+            ])
+        ));
+
+        $apiKey = 'api key value';
+
+        $this->client->createFileSource($apiKey, $label);
+
+        $request = $this->getLastRequest();
+        self::assertSame('POST', $request->getMethod());
+        self::assertSame('Bearer ' . $apiKey, $request->getHeaderLine('authorization'));
+    }
+
+    public function testCreateFileSourceInvalidErrorResponse(): void
+    {
+        $this->doClientActionThrowsInvalidModelDataException(
+            400,
+            function () {
+                $this->client->createFileSource('token value', 'label value');
+            },
+            InvalidRequestError::class,
+        );
+    }
+
+    protected function createClientActionCallable(): callable
+    {
+        return function () {
+            $this->client->createFileSource('api token', 'label');
+        };
+    }
+
+    protected function getExpectedModelClass(): string
+    {
+        return FileSource::class;
+    }
+}
