0b99dc633e12590aeb793fb9798fa7c3063e5fe1
Jon Cram <webignition@gmail.com>
2023-02-06 16:58:01 +0000

    Add functional tests for file source creation (#23)

---
+++ tests/Functional/Client/AbstractClientTestCase.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\SourcesClient\Tests\Functional\Client;
+
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\HttpFactory;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use SmartAssert\ServiceClient\Client as ServiceClient;
+use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
+use SmartAssert\SourcesClient\Client;
+use SmartAssert\SourcesClient\InvalidRequestErrorFactory;
+use SmartAssert\SourcesClient\InvalidRequestFieldFactory;
+use SmartAssert\SourcesClient\Tests\Functional\DataProvider\CommonNonSuccessResponseDataProviderTrait;
+use SmartAssert\SourcesClient\Tests\Functional\DataProvider\InvalidJsonResponseExceptionDataProviderTrait;
+use SmartAssert\SourcesClient\Tests\Functional\DataProvider\NetworkErrorExceptionDataProviderTrait;
+use webignition\HttpHistoryContainer\Container as HttpHistoryContainer;
+
+abstract class AbstractClientTestCase extends TestCase
+{
+    use CommonNonSuccessResponseDataProviderTrait;
+    use InvalidJsonResponseExceptionDataProviderTrait;
+    use NetworkErrorExceptionDataProviderTrait;
+
+    protected MockHandler $mockHandler;
+    protected Client $client;
+    private HttpHistoryContainer $httpHistoryContainer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockHandler = new MockHandler();
+
+        $httpFactory = new HttpFactory();
+
+        $handlerStack = HandlerStack::create($this->mockHandler);
+
+        $this->httpHistoryContainer = new HttpHistoryContainer();
+        $handlerStack->push(Middleware::history($this->httpHistoryContainer));
+
+        $this->client = new Client(
+            'https://sources.example.com',
+            new ServiceClient(
+                $httpFactory,
+                $httpFactory,
+                new HttpClient(['handler' => $handlerStack]),
+            ),
+            new InvalidRequestErrorFactory(
+                new InvalidRequestFieldFactory(),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider networkErrorExceptionDataProvider
+     * @dataProvider invalidJsonResponseExceptionDataProvider
+     *
+     * @param class-string<\Throwable> $expectedExceptionClass
+     */
+    public function testClientActionThrowsException(
+        ResponseInterface|ClientExceptionInterface $httpFixture,
+        string $expectedExceptionClass,
+    ): void {
+        $this->mockHandler->append($httpFixture);
+
+        $this->expectException($expectedExceptionClass);
+
+        ($this->createClientActionCallable())();
+    }
+
+    /**
+     * @dataProvider commonNonSuccessResponseDataProvider
+     */
+    public function testClientActionThrowsNonSuccessResponseException(ResponseInterface $httpFixture): void
+    {
+        $this->mockHandler->append($httpFixture);
+
+        try {
+            ($this->createClientActionCallable())();
+
+            self::fail(NonSuccessResponseException::class . ' not thrown');
+        } catch (NonSuccessResponseException $e) {
+            self::assertSame($httpFixture, $e->response);
+        }
+    }
+
+    protected function getLastRequest(): RequestInterface
+    {
+        $request = $this->httpHistoryContainer->getTransactions()->getRequests()->getLast();
+        \assert($request instanceof RequestInterface);
+
+        return $request;
+    }
+
+    abstract protected function createClientActionCallable(): callable;
+}
