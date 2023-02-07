61a945fb4a485e7c174f7f1d5eef5ad71ab34d80
Jon Cram <webignition@gmail.com>
2023-02-07 17:47:23 +0000

    Create git source (#26)

--- src/Client.php
+++ src/Client.php
@@ -14,7 +14,9 @@ use SmartAssert\ServiceClient\Exception\InvalidResponseDataException;
 use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
 use SmartAssert\ServiceClient\Payload\UrlEncodedPayload;
 use SmartAssert\ServiceClient\Request;
+use SmartAssert\ServiceClient\Response\JsonResponse;
 use SmartAssert\SourcesClient\Model\FileSource;
+use SmartAssert\SourcesClient\Model\GitSource;
 use SmartAssert\SourcesClient\Model\InvalidRequestError;
 
 class Client
@@ -47,16 +49,7 @@ class Client
         );
 
         if (400 === $response->getStatusCode()) {
-            $invalidRequestError = $this->invalidRequestErrorFactory->create(
-                $response->getHttpResponse(),
-                new ArrayInspector($response->getData())
-            );
-
-            if (null === $invalidRequestError) {
-                throw InvalidModelDataException::fromJsonResponse(InvalidRequestError::class, $response);
-            }
-
-            return $invalidRequestError;
+            return $this->createInvalidRequestError($response);
         }
 
         if (!$response->isSuccessful()) {
@@ -76,6 +69,65 @@ class Client
     }
 
     /**
+     * @param non-empty-string  $label
+     * @param non-empty-string  $token
+     * @param non-empty-string  $hostUrl
+     * @param non-empty-string  $path
+     * @param ?non-empty-string $credentials
+     *
+     * @throws ClientExceptionInterface
+     * @throws InvalidResponseContentException
+     * @throws InvalidResponseDataException
+     * @throws NonSuccessResponseException
+     * @throws InvalidModelDataException
+     */
+    public function createGitSource(
+        string $token,
+        string $label,
+        string $hostUrl,
+        string $path,
+        ?string $credentials,
+    ): GitSource|InvalidRequestError {
+        $payload = [
+            'label' => $label,
+            'host-url' => $hostUrl,
+            'path' => $path,
+        ];
+
+        if (is_string($credentials)) {
+            $payload['credentials'] = $credentials;
+        }
+
+        $response = $this->serviceClient->sendRequestForJsonEncodedData(
+            (new Request('POST', $this->createUrl('/git')))
+                ->withAuthentication(new BearerAuthentication($token))
+                ->withPayload(new UrlEncodedPayload($payload))
+        );
+
+        if (400 === $response->getStatusCode()) {
+            return $this->createInvalidRequestError($response);
+        }
+
+        if (!$response->isSuccessful()) {
+            throw new NonSuccessResponseException($response->getHttpResponse());
+        }
+
+        $responseDataInspector = new ArrayInspector($response->getData());
+
+        $label = $responseDataInspector->getNonEmptyString('label');
+        $hostUrl = $responseDataInspector->getNonEmptyString('host_url');
+        $path = $responseDataInspector->getNonEmptyString('path');
+        $id = $responseDataInspector->getNonEmptyString('id');
+        $hasCredentials = $responseDataInspector->getBoolean('has_credentials');
+
+        if (null === $label || null === $hostUrl || null === $path || null === $id || null === $hasCredentials) {
+            throw InvalidModelDataException::fromJsonResponse(GitSource::class, $response);
+        }
+
+        return new GitSource($label, $hostUrl, $path, $hasCredentials, $id);
+    }
+
+    /**
      * @param non-empty-string $path
      *
      * @return non-empty-string
@@ -83,5 +135,24 @@ class Client
     private function createUrl(string $path): string
     {
         return rtrim($this->baseUrl, '/') . $path;
+    }
+
+    /**
+     * @throws InvalidModelDataException
+     * @throws InvalidResponseContentException
+     * @throws InvalidResponseDataException
+     */
+    private function createInvalidRequestError(JsonResponse $response): InvalidRequestError
+    {
+        $invalidRequestError = $this->invalidRequestErrorFactory->create(
+            $response->getHttpResponse(),
+            new ArrayInspector($response->getData())
+        );
+
+        if (null === $invalidRequestError) {
+            throw InvalidModelDataException::fromJsonResponse(InvalidRequestError::class, $response);
+        }
+
+        return $invalidRequestError;
     }
 }
---
+++ tests/Integration/CreateGitSourceTest.php
@@ -0,0 +1,173 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\SourcesClient\Tests\Integration;
+
+use SmartAssert\SourcesClient\Model\GitSource;
+use SmartAssert\SourcesClient\Model\InvalidRequestError;
+use SmartAssert\SourcesClient\Model\InvalidRequestField;
+
+class CreateGitSourceTest extends AbstractIntegrationTestCase
+{
+    /**
+     * @dataProvider createGitSourceInvalidRequestDataProvider
+     *
+     * @param non-empty-string      $label
+     * @param non-empty-string      $hostUrl
+     * @param non-empty-string      $path
+     * @param InvalidRequestField[] $expectedInvalidRequestFields
+     */
+    public function testCreateGitSourceInvalidRequest(
+        string $label,
+        string $hostUrl,
+        string $path,
+        array $expectedInvalidRequestFields
+    ): void {
+        $invalidRequestError = self::$client->createGitSource(
+            self::$user1ApiToken->token,
+            $label,
+            $hostUrl,
+            $path,
+            null
+        );
+
+        self::assertInstanceOf(InvalidRequestError::class, $invalidRequestError);
+        self::assertEquals($expectedInvalidRequestFields, $invalidRequestError->invalidRequestFields);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function createGitSourceInvalidRequestDataProvider(): array
+    {
+        $label = 'git source label';
+        $labelTooLong = str_repeat('.', 256);
+        $hostUrl = 'https://example.com/repository.git';
+        $hostUrlTooLong = str_repeat('.', 256);
+        $path = '/';
+        $pathTooLong = str_repeat('.', 256);
+
+        return [
+            'label missing' => [
+                'label' => '  ',
+                'hostUrl' => $hostUrl,
+                'path' => $path,
+                'expectedInvalidRequestFields' => [
+                    'label' => new InvalidRequestField(
+                        'label',
+                        '',
+                        'This value is too short. It should have 1 character or more.'
+                    ),
+                ],
+            ],
+            'label too long' => [
+                'label' => $labelTooLong,
+                'hostUrl' => $hostUrl,
+                'path' => $path,
+                'expectedInvalidRequestFields' => [
+                    'label' => new InvalidRequestField(
+                        'label',
+                        $labelTooLong,
+                        'This value is too long. It should have 255 characters or less.',
+                    ),
+                ],
+            ],
+            'host url missing' => [
+                'label' => $label,
+                'hostUrl' => '   ',
+                'path' => $path,
+                'expectedInvalidRequestFields' => [
+                    'host-url' => new InvalidRequestField(
+                        'host-url',
+                        '',
+                        'This value is too short. It should have 1 character or more.'
+                    ),
+                ],
+            ],
+            'host url too long' => [
+                'label' => $label,
+                'hostUrl' => $hostUrlTooLong,
+                'path' => $path,
+                'expectedInvalidRequestFields' => [
+                    'host-url' => new InvalidRequestField(
+                        'host-url',
+                        $hostUrlTooLong,
+                        'This value is too long. It should have 255 characters or less.',
+                    ),
+                ],
+            ],
+            'path missing' => [
+                'label' => $label,
+                'hostUrl' => $hostUrl,
+                'path' => '  ',
+                'expectedInvalidRequestFields' => [
+                    'path' => new InvalidRequestField(
+                        'path',
+                        '',
+                        'This value is too short. It should have 1 character or more.'
+                    ),
+                ],
+            ],
+            'path too long' => [
+                'label' => $label,
+                'hostUrl' => $hostUrl,
+                'path' => $pathTooLong,
+                'expectedInvalidRequestFields' => [
+                    'path' => new InvalidRequestField(
+                        'path',
+                        $pathTooLong,
+                        'This value is too long. It should have 255 characters or less.',
+                    ),
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider createGitSourceSuccessDataProvider
+     *
+     * @param non-empty-string  $label
+     * @param non-empty-string  $hostUrl
+     * @param non-empty-string  $path
+     * @param ?non-empty-string $credentials
+     */
+    public function testCreateGitSourceSuccess(string $label, string $hostUrl, string $path, ?string $credentials): void
+    {
+        $gitSource = self::$client->createGitSource(
+            self::$user1ApiToken->token,
+            $label,
+            $hostUrl,
+            $path,
+            $credentials
+        );
+
+        self::assertInstanceOf(GitSource::class, $gitSource);
+        self::assertSame($label, $gitSource->label);
+        self::assertSame($hostUrl, $gitSource->hostUrl);
+        self::assertSame($path, $gitSource->path);
+        self::assertSame(is_string($credentials), $gitSource->hasCredentials);
+        self::assertNotEmpty($gitSource->id);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function createGitSourceSuccessDataProvider(): array
+    {
+        return [
+            'without credentials' => [
+                'label' => md5((string) rand()),
+                'hostUrl' => md5((string) rand()),
+                'path' => md5((string) rand()),
+                'credentials' => null,
+            ],
+            'with credentials' => [
+                'label' => md5((string) rand()),
+                'hostUrl' => md5((string) rand()),
+                'path' => md5((string) rand()),
+                'credentials' => md5((string) rand()),
+            ],
+        ];
+    }
+}
