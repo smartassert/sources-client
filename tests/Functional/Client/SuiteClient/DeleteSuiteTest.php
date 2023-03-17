eeb5a7cb446ab18fe9f10cf48f0c62a06f272db3
Jon Cram <webignition@gmail.com>
2023-03-17 15:58:01 +0000

    Remove suite (#104)

---
+++ tests/Functional/Client/SuiteClient/DeleteSuiteTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\SourcesClient\Tests\Functional\Client\SuiteClient;
+
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
+use SmartAssert\SourcesClient\Exception\ResponseException;
+use SmartAssert\SourcesClient\Tests\Functional\DataProvider\InvalidJsonResponseExceptionDataProviderTrait;
+use SmartAssert\SourcesClient\Tests\Functional\DataProvider\NetworkErrorExceptionDataProviderTrait;
+
+class DeleteSuiteTest extends AbstractSuiteClientTest
+{
+    use InvalidJsonResponseExceptionDataProviderTrait;
+    use NetworkErrorExceptionDataProviderTrait;
+
+    public function testDeleteThrowsInvalidModelDataException(): void
+    {
+        $responsePayload = ['key' => 'value'];
+        $response = new Response(
+            400,
+            ['content-type' => 'application/json'],
+            (string) json_encode($responsePayload)
+        );
+
+        $this->mockHandler->append($response);
+
+        try {
+            ($this->createClientActionCallable())();
+            self::fail(InvalidModelDataException::class . ' not thrown');
+        } catch (InvalidModelDataException $e) {
+            self::assertSame(ResponseException::class, $e->class);
+            self::assertSame($response, $e->response);
+            self::assertSame($responsePayload, $e->payload);
+        }
+    }
+
+    public function clientActionThrowsExceptionDataProvider(): array
+    {
+        return array_merge(
+            $this->networkErrorExceptionDataProvider(),
+            $this->invalidJsonResponseExceptionDataProvider(),
+        );
+    }
+
+    protected function createClientActionCallable(): callable
+    {
+        return function () {
+            $this->suiteClient->delete(self::API_KEY, md5((string) rand()));
+        };
+    }
+
+    protected function getExpectedRequestMethod(): string
+    {
+        return 'DELETE';
+    }
+
+    protected function getClientActionSuccessResponse(): ResponseInterface
+    {
+        return new Response(
+            200,
+            ['content-type' => 'application/json'],
+            (string) json_encode([
+                'id' => md5((string) rand()),
+                'source_id' => md5((string) rand()),
+                'label' => md5((string) rand()),
+                'tests' => [md5((string) rand()) . '.yaml'],
+            ])
+        );
+    }
+}
