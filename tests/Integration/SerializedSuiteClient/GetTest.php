b5fd8b2f7c9d9bd701930738b1c9ff4b00f800c2
Jon Cram <webignition@gmail.com>
2023-03-20 16:10:00 +0000

    Get serialized suite

--- tests/Integration/SerializedSuiteClient/GetTest.php
+++ tests/Integration/SerializedSuiteClient/GetTest.php
@@ -9,14 +9,14 @@ use SmartAssert\SourcesClient\Model\SourceInterface;
 use SmartAssert\SourcesClient\Tests\DataProvider\GetSuiteDataProviderTrait;
 use SmartAssert\SourcesClient\Tests\Integration\AbstractIntegrationTestCase;
 
-class CreateTest extends AbstractIntegrationTestCase
+class GetTest extends AbstractIntegrationTestCase
 {
     use GetSuiteDataProviderTrait;
 
-    public function testCreateSuiteNotFound(): void
+    public function testGetSerializedSuiteNotFound(): void
     {
         try {
-            self::$serializedSuiteClient->create(self::$user1ApiToken->token, md5((string) rand()));
+            self::$serializedSuiteClient->get(self::$user1ApiToken->token, md5((string) rand()));
         } catch (\Throwable $e) {
             self::assertInstanceOf(NonSuccessResponseException::class, $e);
             self::assertSame(404, $e->getCode());
@@ -30,7 +30,7 @@ class CreateTest extends AbstractIntegrationTestCase
      * @param array<non-empty-string, non-empty-string> $parameters
      * @param array<non-empty-string, non-empty-string> $expectedSerializedSuiteParameters
      */
-    public function testCreateSuccess(
+    public function testGetSuccess(
         callable $sourceCreator,
         array $parameters,
         array $expectedSerializedSuiteParameters,
@@ -44,17 +44,32 @@ class CreateTest extends AbstractIntegrationTestCase
             ['Test1.yaml', 'Test2.yaml']
         );
 
-        $serializedSuite = self::$serializedSuiteClient->create(
+        $createdSerializedSuite = self::$serializedSuiteClient->create(
             self::$user1ApiToken->token,
             $suite->getId(),
             $parameters
         );
 
+        $serializedSuite = self::$serializedSuiteClient->get(
+            self::$user1ApiToken->token,
+            $createdSerializedSuite->getId()
+        );
+
         self::assertNotNull($serializedSuite->getId());
         self::assertSame($suite->getId(), $serializedSuite->getSuiteId());
         self::assertSame($expectedSerializedSuiteParameters, $serializedSuite->getParameters());
-        self::assertSame('requested', $serializedSuite->getState());
         self::assertNull($serializedSuite->getFailureReason());
         self::assertNull($serializedSuite->getFailureMessage());
+
+        $expectedStates = ['requested', 'preparing/running', 'preparing/halted'];
+
+        self::assertTrue(
+            in_array($serializedSuite->getState(), $expectedStates),
+            sprintf(
+                'Serialized suite state "%s" is not one of %s',
+                $serializedSuite->getState(),
+                implode(', ', $expectedStates)
+            )
+        );
     }
 }
