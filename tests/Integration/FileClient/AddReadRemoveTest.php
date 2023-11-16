22a1ab9cb2bd2b111484be6e60c6fbce17d52033
Jon Cram <webignition@gmail.com>
2023-11-16 17:06:00 +0000

    Use DuplicateFilePathException

--- tests/Integration/FileClient/AddReadRemoveTest.php
+++ tests/Integration/FileClient/AddReadRemoveTest.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 namespace SmartAssert\SourcesClient\Tests\Integration\FileClient;
 
 use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
+use SmartAssert\SourcesClient\Exception\DuplicateFilePathException;
 use SmartAssert\SourcesClient\Model\FileSource;
 use SmartAssert\SourcesClient\Tests\Integration\AbstractIntegrationTestCase;
 
 class AddReadRemoveTest extends AbstractIntegrationTestCase
 {
+    public function testAddDuplicateFilePath(): void
+    {
+        $label = md5((string) rand());
+
+        $fileSource = self::$fileSourceClient->create(self::$user1ApiToken->token, $label);
+        self::assertInstanceOf(FileSource::class, $fileSource);
+
+        $filename = md5((string) rand()) . '.yaml';
+        $content = md5((string) rand());
+
+        self::$fileClient->add(self::$user1ApiToken->token, $fileSource->getId(), $filename, $content);
+
+        self::expectException(DuplicateFilePathException::class);
+        self::$fileClient->add(self::$user1ApiToken->token, $fileSource->getId(), $filename, $content);
+    }
+
     public function testAddFileReadFile(): void
     {
         $label = md5((string) rand());
