eb5d5fbf145269ea47df95f9e645f079ca20b693
Jon Cram <webignition@gmail.com>
2024-03-04 12:37:26 +0000

    Remove all but FileSourceClientInterface::create()

--- src/FileSourceClientInterface.php
+++ src/FileSourceClientInterface.php
@@ -11,41 +11,10 @@ use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
 use SmartAssert\ServiceClient\Exception\InvalidResponseDataException;
 use SmartAssert\ServiceClient\Exception\InvalidResponseTypeException;
 use SmartAssert\ServiceClient\Exception\UnauthorizedException;
-use SmartAssert\SourcesClient\Exception\ModifyReadOnlyEntityException;
 use SmartAssert\SourcesClient\Model\FileSource;
 
 interface FileSourceClientInterface
 {
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $sourceId
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
-    public function get(string $token, string $sourceId): FileSource;
-
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $fileSourceId
-     *
-     * @return string[]
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
-    public function list(string $token, string $fileSourceId): array;
-
     /**
      * @param non-empty-string $token
      *
@@ -58,33 +27,4 @@ interface FileSourceClientInterface
      * @throws UnauthorizedException
      */
     public function create(string $token, string $label): FileSource;
-
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $sourceId
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws ModifyReadOnlyEntityException
-     * @throws UnauthorizedException
-     */
-    public function update(string $token, string $sourceId, string $label): FileSource;
-
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $sourceId
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
-    public function delete(string $token, string $sourceId): FileSource;
 }
