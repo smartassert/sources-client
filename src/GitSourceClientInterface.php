14fd00dec209960f8c84134016368fd346582a6e
Jon Cram <webignition@gmail.com>
2024-03-04 12:33:29 +0000

    Remove all but GitSourceClientInterface::create()

--- src/GitSourceClientInterface.php
+++ src/GitSourceClientInterface.php
@@ -11,25 +11,10 @@ use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
 use SmartAssert\ServiceClient\Exception\InvalidResponseDataException;
 use SmartAssert\ServiceClient\Exception\InvalidResponseTypeException;
 use SmartAssert\ServiceClient\Exception\UnauthorizedException;
-use SmartAssert\SourcesClient\Exception\ModifyReadOnlyEntityException;
 use SmartAssert\SourcesClient\Model\GitSource;
 
 interface GitSourceClientInterface
 {
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $sourceId
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
-    public function get(string $token, string $sourceId): GitSource;
-
     /**
      * @param non-empty-string  $token
      * @param ?non-empty-string $credentials
@@ -49,40 +34,4 @@ interface GitSourceClientInterface
         string $path,
         ?string $credentials,
     ): GitSource;
-
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $sourceId
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws ModifyReadOnlyEntityException
-     * @throws UnauthorizedException
-     */
-    public function update(
-        string $token,
-        string $sourceId,
-        string $label,
-        string $hostUrl,
-        string $path,
-        ?string $credentials,
-    ): GitSource;
-
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $sourceId
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
-    public function delete(string $token, string $sourceId): GitSource;
 }
