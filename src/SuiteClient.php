5149dd5b42cdb37c948cede3fe90eb89935ac315
Jon Cram <webignition@gmail.com>
2024-03-04 12:32:55 +0000

    Remove all but SuiteClientInterface::create() (#213)

--- src/SuiteClient.php
+++ src/SuiteClient.php
@@ -11,11 +11,9 @@ use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
 use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
 use SmartAssert\ServiceClient\Exception\UnauthorizedException;
 use SmartAssert\ServiceClient\Payload\UrlEncodedPayload;
-use SmartAssert\SourcesClient\Exception\ModifyReadOnlyEntityException;
 use SmartAssert\SourcesClient\Model\Suite;
 use SmartAssert\SourcesClient\Request\RequestInterface;
 use SmartAssert\SourcesClient\Request\SuiteCreationRequest;
-use SmartAssert\SourcesClient\Request\SuiteMutationRequest;
 
 class SuiteClient implements SuiteClientInterface
 {
@@ -30,78 +28,6 @@ class SuiteClient implements SuiteClientInterface
     public function create(string $token, string $sourceId, string $label, array $tests): Suite
     {
         return $this->makeMutationRequest($token, new SuiteCreationRequest($sourceId, $label, $tests));
-    }
-
-    public function get(string $token, string $suiteId): Suite
-    {
-        try {
-            $response = $this->serviceClient->sendRequestForJson(
-                $this->requestFactory->createSuiteRequest('GET', $token, $suiteId)
-            );
-        } catch (NonSuccessResponseException $e) {
-            throw $this->exceptionFactory->createFromResponse($e->getResponse());
-        }
-
-        $suite = $this->suiteFactory->create($response->getData());
-        if (null === $suite) {
-            throw InvalidModelDataException::fromJsonResponse(Suite::class, $response);
-        }
-
-        return $suite;
-    }
-
-    public function update(string $token, string $suiteId, string $sourceId, string $label, array $tests): Suite
-    {
-        try {
-            return $this->makeMutationRequest($token, new SuiteMutationRequest($sourceId, $label, $tests, $suiteId));
-        } catch (NonSuccessResponseException $e) {
-            if (405 === $e->getCode()) {
-                throw new ModifyReadOnlyEntityException($suiteId, 'suite');
-            }
-
-            throw $e;
-        }
-    }
-
-    public function delete(string $token, string $suiteId): Suite
-    {
-        try {
-            $response = $this->serviceClient->sendRequestForJson(
-                $this->requestFactory->createSuiteRequest('DELETE', $token, $suiteId)
-            );
-        } catch (NonSuccessResponseException $e) {
-            throw $this->exceptionFactory->createFromResponse($e->getResponse());
-        }
-
-        $suite = $this->suiteFactory->create($response->getData());
-        if (null === $suite) {
-            throw InvalidModelDataException::fromJsonResponse(Suite::class, $response);
-        }
-
-        return $suite;
-    }
-
-    public function list(string $token): array
-    {
-        try {
-            $response = $this->serviceClient->sendRequestForJson($this->requestFactory->createSuitesRequest($token));
-        } catch (NonSuccessResponseException $e) {
-            throw $this->exceptionFactory->createFromResponse($e->getResponse());
-        }
-
-        $sources = [];
-
-        foreach ($response->getData() as $sourceData) {
-            if (is_array($sourceData)) {
-                $suite = $this->suiteFactory->create($sourceData);
-
-                if ($suite instanceof Suite) {
-                    $sources[] = $suite;
-                }
-            }
-        }
-
-        return $sources;
     }
 
     /**
--- src/SuiteClientInterface.php
+++ src/SuiteClientInterface.php
@@ -5,13 +5,9 @@ declare(strict_types=1);
 namespace SmartAssert\SourcesClient;
 
 use Psr\Http\Client\ClientExceptionInterface;
-use SmartAssert\ServiceClient\Exception\CurlExceptionInterface;
 use SmartAssert\ServiceClient\Exception\HttpResponseExceptionInterface;
 use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
-use SmartAssert\ServiceClient\Exception\InvalidResponseDataException;
-use SmartAssert\ServiceClient\Exception\InvalidResponseTypeException;
 use SmartAssert\ServiceClient\Exception\UnauthorizedException;
-use SmartAssert\SourcesClient\Exception\ModifyReadOnlyEntityException;
 use SmartAssert\SourcesClient\Model\Suite;
 
 interface SuiteClientInterface
@@ -26,59 +22,4 @@ interface SuiteClientInterface
      * @throws UnauthorizedException
      */
     public function create(string $token, string $sourceId, string $label, array $tests): Suite;
-
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $suiteId
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
-    public function get(string $token, string $suiteId): Suite;
-
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $suiteId
-     * @param string[]         $tests
-     *
-     * @throws ClientExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws ModifyReadOnlyEntityException
-     * @throws UnauthorizedException
-     */
-    public function update(string $token, string $suiteId, string $sourceId, string $label, array $tests): Suite;
-
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $suiteId
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
-    public function delete(string $token, string $suiteId): Suite;
-
-    /**
-     * @param non-empty-string $token
-     *
-     * @return Suite[]
-     *
-     * @throws ClientExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
-    public function list(string $token): array;
 }
